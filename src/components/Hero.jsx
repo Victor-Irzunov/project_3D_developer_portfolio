e7bd05ce0a6d5f9f040dca823015b31c5a7c38d1
Виктор <viktor@MacBook-Air-Viktor.local>
2023-03-25 15:13:02 +0300

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,16 +16,15 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Услуга &nbsp;<span className='text-[#915EFF] block'>муж на час</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+           в Минске
           </p>
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,7 +16,17 @@ module.exports = {
         card: "0px 35px 120px -15px #211e35",
       },
       screens: {
-        xs: "450px",
+        xyy: "318px",
+        xy: "373px",
+        xx: "415px",
+        xs: "480px",
+        xm: "575px",
+        ss: "620px",
+        sm: "768px",
+        sd: "991px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
       },
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
